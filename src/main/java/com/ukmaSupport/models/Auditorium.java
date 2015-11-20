ef5fc1c23fe1c07467628d5ff529723e8925aeef
IgorNo <novikof1@inbox.ru>
2015-11-20 15:13:36 +0200

    Add models

---
+++ src/main/java/com/ukmaSupport/models/Auditorium.java
@@ -0,0 +1,43 @@
+package com.ukmaSupport.models;
+
+import java.io.Serializable;
+
+
+public class Auditorium implements Serializable {
+    private int id;
+    private int userId;
+    private String number;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+    
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Auditorium{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", number='" + number + '\'' +
+                '}';
+    }
+}
